c386a0a2a2da6e095c44b6b99d89ea48ab09b75d
Faiz Samjani <125338514+faizmemon@users.noreply.github.com>
2023-03-04 11:18:35 +0530

    Add files via upload

---
+++ Task/dynamic_division.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+void main()
+{
+    //That is a Dynamic Division
+    int a,b,ans;
+    printf("Enter the value of A and B");
+    scanf("%d%d",&a,&b);
+    ans=a/b;
+    printf("Division of A and B = %d",ans);
+}
