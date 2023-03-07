f30cbc9853b60589b75d7f320d85b7bc7ba86acc
Faiz Samjani <125338514+faizmemon@users.noreply.github.com>
2023-03-07 13:27:07 +0530

    Add files via upload

---
+++ Assingment/Module - 3.1/module3_1que1.c
@@ -0,0 +1,10 @@
+//Write a program and Type My name,Birth date,Age and Address
+#include<stdio.h>
+void main()
+{
+    printf("\n\t1)NAME :- Samjani Mohammadfaiz Arifbhai");
+    printf("\n\t2)Birth Date :- 24-December-2001");
+    printf("\n\t3)Age :- 21 Year  (According to Year 2023)");
+    printf("\n\t4)Address :- Bhagat Geat Road,\n\t\t\tVillage - Vasavad/364490 ,\n\t\t\tTaluka - Gondal ,\n\t\t\tDistrict - Rajkot.");
+    
+}
