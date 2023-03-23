83ccdd96318867ff21b1b2d7fde2a1a13b43a934
Faiz Samjani <125338514+faizmemon@users.noreply.github.com>
2023-03-23 10:06:25 +0530

    Add files via upload

---
+++ Assingment/Module - 3.2/module3_2Que4-2.c
@@ -0,0 +1,40 @@
+// Module 3.2 Que. 4 (2).
+// Vowel or Consonant using switch case 
+
+#include<stdio.h>
+void main()
+
+{
+    char abcd;
+    printf("Enter The Character - ");
+    scanf("%c",&abcd);
+
+    switch (abcd)
+    {
+    case 'A':
+
+    case 'a':
+
+    case 'E':
+
+    case 'e':
+
+    case 'I':
+
+    case 'i':
+
+    case 'O':
+
+    case 'o':
+
+    case 'U':
+
+    case 'u':
+            printf("\n This Character is Vowels");
+        break;
+    
+    default:
+            printf("\n This Character is Consonents");
+        break;
+    }
+}
