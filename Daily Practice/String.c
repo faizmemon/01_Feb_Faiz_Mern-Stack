94921b0f70df98441a4eba317c3933bae065b173
Faiz Samjani <125338514+faizmemon@users.noreply.github.com>
2023-03-23 12:34:58 +0530

    Add files via upload

---
+++ Daily Practice/String.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+void main()
+
+{
+    /*
+    // char [10] = {'a', 'b', 'c', 'd', 'e', 'f',};
+    // char str [10] = {"abcde"};
+    printf("\nString is : %s", str);
+    */
+   char string[5];
+   printf("Enter Your Name -");
+   scanf("%s",&string);
+   printf("\nWelcome %s to Our Company ");
+   printf("\n\n\t\t - THE EAGLE GROUP OF TECHNOLOGY - ");
+   printf("\n\n\t\t\tPlease Welcome");
+}
